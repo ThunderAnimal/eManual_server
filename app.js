c1be5ebb826cb282ce82202b1cb773f5f76f0af7
ThunderAnimal <MartinWeber.93@outlook.com>
2017-11-08 14:35:35 +0100

    - improve error handling - adds template layout - adds jquery

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ if(config.util.getEnv('NODE_ENV') !== 'test') {
 //Connect Databse
 mongoose.connect(config.database.connectionURL);
 var db = mongoose.connection;
+
 db.on('error', function (error) {
     console.error("MongoDB connection error:");
     console.error(error);
@@ -49,9 +50,9 @@ db.on('open', function () {
 
 //Middelware - Logging
 app.use(function (req, res, next) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('Client IP:', ip);
-    next();
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('Client IP:', ip);
+  next();
 });
 
 
@@ -65,32 +66,33 @@ app.use('/api/v1', require('./routes/api_v1'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    res.status(err.status || 500);
-    res.send(req.app.get('env') === 'development' ? err.message : "Error!");
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 function allowAccessAllowOrigin  (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 }
 
 function isAuthorized(req, res, next){
-    res.status(401).send({
-        status: 401,
-        message: "Not Authorized."
-    });
+  var err = new Error('Not Authorized');
+  err.status = 401;
+
+  next(err);
 }
 
 module.exports = app;
