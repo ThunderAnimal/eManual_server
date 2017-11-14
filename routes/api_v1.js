e794b5b519c6e697bbd8f007a01c66da1ac7393d
PJ <prasannjeetsingh@gmail.com>
2017-11-14 01:54:19 +0100

    Got the list of users successfully.
    Was able to get the list of users successfully, but they are only visible in the console. Don't know how to send the response to the web page.

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -2,12 +2,33 @@ var express = require('express');
 var router = express.Router();
 var rep = require('../app/models/Representative');
 var policy = require('../app/moduls/routePolicy');
+let context = {
+    status:null,
+};
+var result = null;
+
 
 router.get('/representatives', policy.onlyCompanyAllowed, function (req, res, next) {
-    //How to get the id from the company
-    console.log(req.user._id);
-    //TODO GET DATA FROM DATABASE
-    res.send({data: req.user.name});
+    // //How to get the id from the company
+    // console.log(req.user._id);
+    // //TODO GET DATA FROM DATABASE
+    //res.send({data: req.user.name});
+    rep.Representative.find({"company": req.user._id},{name:1},function (err, data) {
+        data.forEach(function(item){
+            console.log(item.name);
+        });
+        result = data;
+    }).then(function (resolve, reject) {
+        if(!req.session.user){
+            context.status = "You are not logged in.";
+            res.render("./",context)
+        }else{
+            context.status = "Hello "+ req.session.user;
+            res.render("./",context);
+        }
+    }).catch(function(){
+        console.log("Catching some exception");
+    });
 });
 
 router.post('/representatives/create', policy.onlyCompanyAllowed, function (req, res, next) {
