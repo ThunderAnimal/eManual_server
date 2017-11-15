2d411743223c6c6a39373e6f98fa8b18b0de7440
ThunderAnimal <MartinWeber.93@outlook.com>
2017-11-15 12:02:33 +0100

    bugfix router policy

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -2,37 +2,40 @@ var express = require('express');
 var router = express.Router();
 var rep = require('../app/models/Representative');
 var policy = require('../app/moduls/routePolicy');
-var context;
 
 
-router.route('/representatives', policy.onlyCompanyAllowed)
+//API representatives
+router.route('/representatives')
+    .all(policy.onlyCompanyAllowed)
     .get( function (req, res, next) {
-    // //How to get the id from the company
-    // console.log(req.user._id);
+        rep.find({"company": req.user._id},{name:1},function (err, data) {
+            if(err){
+                console.log(err);
+            }
+            res.send(data);
+        }).catch(function(e){
+            var err = new Error(e);
+            err.status = 500;
+            next(err);
+        });
+    })
+    .post(function (req, res, next) {
+        var NewRep = new rep({
+            login: req.body.login,
+            password: req.body.password,
+            name: req.body.name,
+            company: req.user._id
+        });
 
-    rep.find({"company": req.user._id},{name:1},function (err, data) {
-        context = data;
-    }).then(function (resolve, reject) {
-            res.send(context);
-    }).catch(function(){
-        console.log("Catching some exception");
-    });
-}).post(function (req, res, next) {
-    //How to get the id from the company;
-    var NewRep = new rep({
-        login : req.body.login,
-        password: req.body.password,
-        name: req.body.name,
-        company: req.user._id
+        NewRep.save().then(function (err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.status(200).send(result);
+            }
+        });
     });
 
-    NewRep.save().then(function (err,result) {
-        if(err){
-            res.send(err);
-        }else {
-            res.status(200).send(result);
-        }
-    });
+//API Products
 
-});
 module.exports = router;
