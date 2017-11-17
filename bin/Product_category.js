611c5194a78b109b31e67d95dd22d29a70ec1adf
eyuel <eyueltb@gmail.com>
2017-11-17 19:58:40 +0100

    just try

---
+++ bin/Product_category.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var config = require("config");
+var prompt = require('prompt');
+var CategoryModel = require('../app/models/Category');
+
+
+//
+// Start the prompt
+//
+prompt.start();
+mongoose.connect(config.database.connectionURL,{ useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+prompt.get('categoryName',function (err,result) {
+
+    var category = new CategoryModel({
+        name: result.categoryName
+    });
+    category.save(function(){
+        console.log("The category is now created.")
+        process.exit(0);
+    });
+});
