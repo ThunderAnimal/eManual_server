08f422124d26b48ab617017dfe31a4ef53f11528
ThunderAnimal <MartinWeber.93@outlook.com>
2017-11-20 08:44:41 +0100

    implements logic to store product data in mongodb

--- app/moduls/ProductManager.js
+++ app/moduls/ProductManager.js
@@ -1,5 +1,7 @@
 var authManager = require("./authManager");
 
+var productModel = require("../models/Product");
+
 exports.getOne = function(req, res, next) {
     next(new Error('not implemented'));
 };
@@ -11,14 +13,44 @@ exports.getAll = function(req, res, next){
 exports.create = function(req, res, next){
     const companyId = authManager.getCompanyId(req.user);
 
+    const images = req.files.image;
+    const resources = req.files.resoruces;
+
     console.log(companyId);
     console.log(req.files);
     console.log(req.body);
-    //TODO Create new Product in MongoDB with the Data
 
-    return res.status(201).send({ok: true});
+    //TODO Strore the uplaod files somwhere and save the link in the mongodb as well
+    var dummyImgageRefList = [];
+    var dummyResourcesRefList = [];
+    if(images){
+        for(var i = 0; i < images.length; i++){
+            dummyImgageRefList.push(images[i].originalname);
+        }
+    }
 
-    //next(new Error('not implemented'));
+    if(resources){
+        for(var j = 0; j < resources.length; j++){
+            dummyResourcesRefList.push(resources[j].originalname);
+        }
+    }
+
+    var product = new productModel({
+        productName : req.body.name,
+        company_id: companyId,
+        categories: req.body.categories,
+        productImages: dummyImgageRefList,
+        productResources: dummyResourcesRefList
+    });
+
+    product.save(function (err, result) {
+        if(err){
+            console.log(err);
+            res.status(500).send({_error: true, err: err});
+        }else{
+            res.status(201).send(result);
+        }
+    });
 };
 
 exports.update = function(req, res, next){
