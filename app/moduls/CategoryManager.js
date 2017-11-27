af900bd8ed7f8619257a74aad4a3cd0388dcbdb2
ThunderAnimal <MartinWeber.93@outlook.com>
2017-11-28 00:20:39 +0100

    refactor get categories

---
+++ app/moduls/CategoryManager.js
@@ -0,0 +1,157 @@
+const categoryModel = require('../models/Category');
+const productModel = require('../models/Product');
+
+
+exports.getOne = function(req, res) {
+    categoryModel.findOne({_id : req.params.id}, function(err, result) {
+        if(err){
+            console.log(err);
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(result);
+        }
+    });
+};
+
+exports.getAll = function(req, res, next) {
+    categoryModel.find({}, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(result);
+        }
+
+    });
+};
+
+exports.getTopWithCounter = function(req, res, next){
+    let topCatList = [
+        "Televisions",
+        "Bestsellers",
+        "Projectors",
+        "Home Entertainment Systems",
+        "Cameras"
+    ];
+
+
+
+    function addCategoryNames(done){
+
+        let finalCatArray = [];
+        const helperAddCategoryNames = function (counter) {
+            if(counter === topCatList.length){
+                done(finalCatArray);
+                return;
+            }
+
+            categoryModel.findOne({"name": topCatList[counter]}, function (err, cat) {
+                if (err) {
+                    console.log(err);
+                    counter += 1;
+                    helperAddCategoryNames(counter);
+                }else{
+                    finalCatArray.push({
+                        _id: cat._id,
+                        name: cat.name,
+                        count: 0
+                    });
+                    counter += 1;
+                    helperAddCategoryNames(counter);
+                }
+            });
+        };
+
+        helperAddCategoryNames(0);
+    }
+
+    addCategoryNames(function(finalCatArray){
+        productModel.find({}, function(err, data){
+            if (err){
+                console.log("Error!!");
+                res.status(500).send(err);
+            }
+            for (let i=0; i<finalCatArray.length; i++){
+                for (let j=0; j<data.length; j++){
+                    for (let k=0; k<data[j].categories.length; k++){
+                        if (finalCatArray[i]._id.toString() === data[j].categories[k].toString()){
+                            finalCatArray[i].count+=1;
+                            break;
+                        }
+                    }
+                }
+            }
+            res.status(200).send(finalCatArray);
+        })
+    });
+};
+
+exports.getAllFromProducts = function (product_list, done) {
+    function addCategoryCounterArray(categoriesList){
+
+        let returnList = [];
+        let found = false;
+        for (let i=0; i<categoriesList.length; i++){
+            found = false;
+            for (let j=0; j<returnList.length; j++){
+                if (returnList[j]._id.toString() === categoriesList[i].toString()){
+                    returnList[j].count +=1;
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                returnList.push({
+                    _id: categoriesList[i],
+                    count: 1
+                });
+            }
+        }
+
+        return returnList;
+    }
+
+    function addCategoryNames(categorieList, done){
+
+        let returnList = [];
+        const helperAddCategoryNames = function (counter) {
+            if(counter === categorieList.length){
+                done(returnList);
+                return;
+            }
+
+            categoryModel.findOne({"_id": categorieList[counter]._id}, function (err, cat) {
+                if (err) {
+                    console.log(err);
+                    counter += 1;
+                    helperAddCategoryNames(counter);
+                }else{
+                    returnList.push({
+                        _id: cat._id,
+                        name: cat.name,
+                        count: categorieList[counter].count
+                    });
+                    counter += 1;
+                    helperAddCategoryNames(counter);
+                }
+            });
+        };
+
+        helperAddCategoryNames(0);
+    }
+
+
+    let cat_collection = [];
+
+    for (let i = 0; i < product_list.length; i++) {
+        for (let j = 0; j < product_list[i].categories.length; j++) {
+            cat_collection.push(product_list[i].categories[j]);
+        }
+    }
+
+    cat_collection = addCategoryCounterArray(cat_collection);
+
+    addCategoryNames(cat_collection, function (cat_result) {
+        done(cat_result);
+    });
+};
--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,33 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var rep = require('../app/models/Representative');
-var policy = require('../app/moduls/routePolicy');
+const express = require('express');
+const router = express.Router();
+const rep = require('../app/models/Representative');
+const policy = require('../app/moduls/routePolicy');
 
-var upload = require('../app/moduls/fileUpload');
+const upload = require('../app/moduls/fileUpload');
 
-var categoryModel = require('../app/models/Category');
-var productModel = require('../app/models/Product');
-var consumerManager = require('../app/moduls/ConsumerManager');
-var productManager = require('../app/moduls/ProductManager');
+const productManager = require('../app/moduls/ProductManager');
+const categoryManager = require('../app/moduls/CategoryManager');
 
 //API representatives
 router.route('/representatives')
-    .all(policy.onlyCompanyAllowed)
+    .all(policy.isAuthorized, policy.onlyCompanyAllowed)
     .get( function (req, res, next) {
-
         rep.find({"company": req.user._id},{name:1},function (err, data) {
-            if(err){
+            if(err) {
                 console.log(err);
+                res.status(500).send(err);
             }
-            res.send(data);
-        }).catch(function(e){
-            var err = new Error(e);
-            err.status = 500;
-            next(err);
+            res.status(200).send(data);
         });
     })
     .post(function (req, res, next) {
-        var NewRep = new rep({
+        const NewRep = new rep({
             login: req.body.login,
             password: req.body.password,
             name: req.body.name,
@@ -36,136 +30,25 @@ router.route('/representatives')
 
         NewRep.save().then(function (err, result) {
             if (err) {
-                res.send(err);
+                console.log(err);
+                res.status(500).send(err);
             } else {
                 res.status(200).send(result);
             }
         });
     });
 
-//API Customer
-
-
 //API Products
 router.get('/products', productManager.getAll);
-router.get('/product/:_id', productManager.getOne);
+router.get('/product/:id', productManager.getOne);
 router.post('/product', policy.isAuthorized, policy.onlyRepresentativeAllowed, upload.fields([{ name: 'image'}, { name: 'resources'}]), productManager.create);
-router.put('/product/:_id', policy.onlyRepresentativeAllowed, productManager.update);
-router.delete('/product/:_id', policy.onlyRepresentativeAllowed, productManager.delete);
+router.put('/product/:id',policy.isAuthorized, policy.onlyRepresentativeAllowed, productManager.update);
+router.delete('/product/:id',policy.isAuthorized, policy.onlyRepresentativeAllowed, productManager.delete);
 
 
-
-router.get('/categories', function(req, res){
-
-
-    function addCategoryCounterArray(categoriesList){
-
-        let returnList = [];
-        let found = false;
-        for (let i=0; i<categoriesList.length; i++){
-            found = false;
-            for (let j=0; j<returnList.length; j++){
-                if (returnList[j]._id.toString() === categoriesList[i].toString()){
-                    returnList[j].count +=1;
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                returnList.push({
-                    _id: categoriesList[i],
-                    count: 1
-                });
-            }
-        }
-
-        return returnList;
-    }
-
-    function addCategoryNames(categorieList, done){
-
-        let returnList = [];
-        const helperAddCategoryNames = function (counter) {
-            if(counter === categorieList.length){
-                done(returnList);
-                return;
-            }
-
-            categoryModel.findOne({"_id": categorieList[counter]._id}, function (err, cat) {
-                if (err) {
-                    console.log(err);
-                    counter += 1;
-                    helperAddCategoryNames(counter);
-                }else{
-                    returnList.push({
-                        _id: cat._id,
-                        name: cat.name,
-                        count: categorieList[counter].count
-                    });
-                    counter += 1;
-                    helperAddCategoryNames(counter);
-                }
-            });
-        };
-
-        helperAddCategoryNames(0);
-    }
-
-    let categoriesList = [];
-    let cat_collection = [];
-    let query_products;
-
-    let cat_list = req.body.categorie_list;
-    cat_list = ['5a1ab6230680f03fe02259da'];
-    if(!cat_list){
-        query_products = {};
-    }else{
-        query_products = {"categories": {"$in" : cat_list}};
-    }
-
-
-    productModel.find(query_products, function (err, product_result) {
-        if (err) {
-            console.log(err);
-            const err = new Error(err);
-            err.status = 500;
-            next(err);
-        }
-
-        for (let i = 0; i < product_result.length; i++) {
-            for (let j = 0; j < product_result[i].categories.length; j++) {
-                cat_collection.push(product_result[i].categories[j]);
-            }
-        }
-        console.log(product_result);
-
-        console.log(cat_collection);
-
-        categoriesList = addCategoryCounterArray(cat_collection);
-
-        console.log(categoriesList);
-
-        for(let k = 0; k < categoriesList.length; k++){
-            for(let j = 0; j < cat_list.length; j++) {
-                if (categoriesList[k]._id.toString() === cat_list[j].toString()) {
-                    console.log(k);
-                    categoriesList.splice(k, 1);
-                }
-            }
-        }
-
-        console.log(categoriesList);
-
-        addCategoryNames(categoriesList, function (cat_result) {
-            res.send({productList: product_result, categoryList: cat_result});
-        });
-    }).catch(function (e) {
-        const err = new Error(e);
-        err.status = 500;
-        next(err);
-    });
-});
-
+//API Categories
+router.get('/categories', categoryManager.getAll);
+router.get('/category/:id', categoryManager.getOne);
 /*
 Currently the top categories are:
 1. Televisions
@@ -174,64 +57,37 @@ Currently the top categories are:
 4. Home Entertainment Systems
 5. Cameras
  */
-router.get('/categories_top', function(req, res){
-    let topCatList = [
-        "Televisions",
-        "Bestsellers",
-        "Projectors",
-        "Home Entertainment Systems",
-        "Cameras"
-    ];
+router.get('/categories_top', categoryManager.getTopWithCounter);
 
 
+//API Combined
+router.get('/dir_products', function(req, res){
 
-    function addCategoryNames(done){
-
-        let finalCatArray = [];
-        const helperAddCategoryNames = function (counter) {
-            if(counter === topCatList.length){
-                done(finalCatArray);
-                return;
-            }
-
-            categoryModel.findOne({"name": topCatList[counter]}, function (err, cat) {
-                if (err) {
-                    console.log(err);
-                    counter += 1;
-                    helperAddCategoryNames(counter);
-                }else{
-                    finalCatArray.push({
-                        _id: cat._id,
-                        name: cat.name,
-                        count: 0
-                    });
-                    counter += 1;
-                    helperAddCategoryNames(counter);
-                }
-            });
-        };
-
-        helperAddCategoryNames(0);
+    let cat_list;
+    if(!req.body.categorie_list){
+        cat_list = [];
+    }else{
+        cat_list = req.body.categorie_list;
     }
 
-    addCategoryNames(function(finalCatArray, done){
-        productModel.find({}, function(err, data){
-            if (err){
-                console.log("Error!!");
-                res.status(500).send(err);
-            }
-            for (let i=0; i<finalCatArray.length; i++){
-                for (let j=0; j<data.length; j++){
-                    for (let k=0; k<data[j].categories.length; k++){
-                        if (finalCatArray[i]._id.toString() === data[j].categories[k].toString()){
-                            finalCatArray[i].count+=1;
-                            break;
-                        }
+    productManager.getAllInCategories(cat_list, function (err, product_result) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+        categoryManager.getAllFromProducts(product_result, function (cat_result) {
+
+            //REMOVE CATEGORIES FROM CALL
+            for(let k = 0; k < cat_result.length; k++){
+                for(let j = 0; j < cat_list.length; j++) {
+                    if (cat_result[k]._id.toString() === cat_list[j].toString()) {
+                        cat_result.splice(k, 1);
                     }
                 }
             }
-            res.status(200).send(finalCatArray);
-        })
+
+            res.status(200).send({productList: product_result, categoryList: cat_result});
+        });
     });
 });
 
