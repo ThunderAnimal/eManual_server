672cb1c63cb92676611737d0d5cb8c410735f16e
PJ <prasannjeetsingh@gmail.com>
2017-12-01 20:18:17 +0100

    modified the API to get the products list

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 const rep = require('../app/models/Representative');
+const consumerModel = require('../app/models/Consumer');
+
 const policy = require('../app/moduls/routePolicy');
 
 const upload = require('../app/moduls/fileUpload');
@@ -8,6 +11,7 @@ const upload = require('../app/moduls/fileUpload');
 const productManager = require('../app/moduls/ProductManager');
 const categoryManager = require('../app/moduls/CategoryManager');
 const consumerManager = require('../app/moduls/ConsumerManager');
+const fs = require('fs');
 
 
 //API representatives
@@ -57,6 +61,8 @@ router.get('/category/:id', categoryManager.getOne);
 
 router.get('/selected_product',policy.isAuthorized,policy.onlyCustomerAllowed,consumerManager.getSelectedProduct);
 router.put('/selected_product',policy.isAuthorized,policy.onlyCustomerAllowed,consumerManager.changeProducts);
+//router.get('/consumerProducts', policy.isAuthorized, policy.onlyCustomerAllowed, consumerManager.getConsumerProducts);
+
 
 /*
 Currently the top categories are:
@@ -72,6 +78,7 @@ router.get('/categories_top', categoryManager.getTopWithCounter);
 //API Combined
 router.get('/dir_products', function(req, res){
     let cat_list;
+
     if(!req.query.categorie_list){
         cat_list = [];
     }else{
@@ -84,13 +91,63 @@ router.get('/dir_products', function(req, res){
             res.status(500).send(err);
         }
         categoryManager.getAllFromProducts(product_result, function (cat_result) {
-
             //REMOVE CATEGORIES FROM CALL
             cat_result = cat_result.filter(function(value){
                 return cat_list.indexOf(value._id.toString()) === -1;
             });
+            let finalProductList = [];
+            let isFavorite = (product_entity, x, req, yes, no) => {
+                let productID = product_entity._id;
+                for (let i = 0; i < req.user.products.length; i++) {
+                    if (req.user.products[i].toString() === productID.toString()) {
+                        yes(x);
+                        return;
+                    }
+                }
+                no(x);
+            };
+            let sendData = (done) => {
+                let isConsumer = () => {
+                    for (let i=0; i<product_result.length; i++){
+                        isFavorite(product_result[i], i, req, (i)=>{
+                            console.log("came to true");
+                            let item = {
+                                _id: product_result[i]._id,
+                                productName: product_result[i].productName,
+                                company_id: product_result[i].company_id,
+                                productResources: product_result[i].productResources,
+                                productImages: product_result[i].productImages,
+                                categories: product_result[i].categories,
+                                isFavorite: true
+                            };
+                            finalProductList.push(item);
+                        }, (i)=>{
+                            console.log("came to false");
+                            let item = {
+                                _id: product_result[i]._id,
+                                productName: product_result[i].productName,
+                                company_id: product_result[i].company_id,
+                                productResources: product_result[i].productResources,
+                                productImages: product_result[i].productImages,
+                                categories: product_result[i].categories,
+                                isFavorite: false
+                            };
+                            finalProductList.push(item);
+                        });
+                    }
+                    done();
+                };
 
-            res.status(200).send({productList: product_result, categoryList: cat_result});
+                let isNotConsumer = () => {
+                    finalProductList = product_result;
+                    done();
+                };
+                consumerManager.isRequestFromUser(req, isConsumer, isNotConsumer);
+
+            };
+            sendData(()=>{
+                res.status(200).send({productList: finalProductList, categoryList: cat_result});
+            });
         });
     });
 });
