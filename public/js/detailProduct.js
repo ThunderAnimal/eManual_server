f220464bbee3148306476b8ebc925ce1499d3998
ThunderAnimal <MartinWeber.93@outlook.com>
2017-12-02 10:21:09 +0100

    showing category list and header in detail page

--- public/js/detailProduct.js
+++ public/js/detailProduct.js
@@ -1,7 +1,39 @@
+var choosenCategories = [];
+
 $(document).ready(function(){
+
+    var category_list = getParams(window.location.href)['category_id[]'];
     var id = $('#product_id').data('id');
+
     getProductData(id, renderProductData);
+    if(category_list){
+        fillChoosenCategories(category_list, function (choosenCategories) {
+            getCatData(choosenCategories,renderCatData);
+            renderNaviagtion(choosenCategories);
+        });
+    }else{
+        getCatData([], renderCatData);
+        renderCatData([]);
+    }
 });
+
+var fillChoosenCategories = function(cat_list, callback){
+    var getCatNameHelper = function(counter){
+        if(counter >= cat_list.length){
+            callback(choosenCategories);
+            return;
+        }
+        $.get('api/v1/category/' + cat_list[counter], function(result){
+            choosenCategories.push({id:result._id, name: result.name});
+            counter = counter + 1;
+            getCatNameHelper(counter);
+        }).fail(function () {
+            counter = counter + 1;
+            getCatNameHelper(counter);
+        });
+    };
+    getCatNameHelper(0);
+}
 
 var getProductData = function(id, callback){
     $.get('api/v1/product/' + id,function (result) {
@@ -17,8 +49,90 @@ var getProductData = function(id, callback){
 };
 
 var renderProductData = function(data){
+    //TODO fill apge with detail data
     console.log(data);
 
     $('#product_name').text(data.productName);
     $('#product_image').attr('src', data.productImages[0]);
 };
+
+var getCatData = function(choosenCategories, callback){
+    var cat_list = [];
+
+    for(var i = 0; i < choosenCategories.length; i++){
+        cat_list.push(choosenCategories[i].id);
+    }
+    $.get('api/v1/dir_products', {categorie_list: cat_list}, function(result){
+        callback(result);
+    });
+};
+
+var renderCatData = function(data){
+    var cats = data.categoryList;
+
+    var linkBase = '/category';
+    if(choosenCategories.length > 0){
+        for(var i = 0; i < choosenCategories.length; i++){
+            if(i === 0){
+                linkBase += '?category_id[]=';
+            }else{
+                linkBase += '&category_id[]=';
+            }
+            linkBase += choosenCategories[i].id;
+        }
+        linkBase += '&category_id[]=';
+    }else{
+        linkBase += '?category_id[]=';
+    }
+
+
+    var cat_list = $("#cat_list");
+    cat_list.empty();
+    cat_list.append('<div class="collection-header center-align"><h4>Categories</h4></div>');
+    for(var i = 0; i < cats.length; i++){
+        cat_list.append('<a href="' + linkBase + cats[i]._id + '" id="' + cats[i]._id + '" name="' + cats[i].name + '" class="collection-item">' + cats[i].name  + '<span class="badge">'+cats[i].count+'</span></a>')
+    }
+};
+
+var renderNaviagtion = function(choosenCategories){
+    var catBreadCrump = $('#category_breadcrumb');
+
+    if(choosenCategories.length <= 0){
+        catBreadCrump.hide();
+        return;
+    }
+
+    catBreadCrump.empty();
+
+    catBreadCrump.append('<a href="/category" id="breadcrump_all" class="breadcrumb">Categories</a>');
+
+    linkBase = '/category' + '?category_id[]=';
+    console.log(choosenCategories);
+    for(var i = 0; i < choosenCategories.length; i++){
+        linkBase += choosenCategories[i].id;
+        catBreadCrump.append('<a href="' + linkBase + '" id="' + choosenCategories[i].id + '" class="breadcrumb">' + choosenCategories[i].name + '</a>');
+        linkBase += '&category_id[]=';
+    }
+
+    catBreadCrump.show();
+};
+
+function getParams(url) {
+    var regex = /([^=&?]+)=([^&#]*)/g, params = {}, parts, key, value;
+
+    while((parts = regex.exec(url)) != null) {
+
+        key = parts[1], value = parts[2];
+        var isArray = /\[\]$/.test(key);
+
+        if(isArray) {
+            params[key] = params[key] || [];
+            params[key].push(value);
+        }
+        else {
+            params[key] = value;
+        }
+    }
+
+    return params;
+}
