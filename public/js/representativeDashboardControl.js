4462a7d922acbd40ca1ba52da44aed088fedd176
weibinyu <a1994218@hotmail.com>
2017-12-08 02:36:50 +0800

    add sort btn

--- public/js/representativeDashboardControl.js
+++ public/js/representativeDashboardControl.js
@@ -1,5 +1,31 @@
+var clicked = false;
 $(document).ready(function(){
     getProductData();
+    $('#count').on('click',function () {
+        if(clicked){
+            console.log("Display in reversed count order")
+        }else
+            console.log("Display in count order")
+    });
+    $('#name').on('click',function () {
+        if(clicked){
+            console.log("Display in reversed name order")
+        }else
+            console.log("Display in name order")
+    });
+    $('#order').on('click',function (e) {
+        if(!clicked){
+            $(this).filter(".materialize-red").removeClass("materialize-red");
+            $(this).addClass('materialize-blue');
+            $(this).text("Reverse");
+            clicked = true;
+        }else if(clicked){
+            $(this).filter(".materialize-blue").removeClass("materialize-blue");
+            $(this).addClass('materialize-red');
+            $(this).text("Order");
+            clicked = false;
+        }
+    })
 });
 
 
@@ -19,6 +45,6 @@ var renderProducts = function(data){
         clonedTemplate.querySelector("h3").innerText = data[product].productName;
         clonedTemplate.querySelector('.pic').src = data[product].profilePicture;
 
-        ul.appendChild(clonedTemplate);;
+        ul.appendChild(clonedTemplate);
     }
 }
