95014d8896c7b498daef2e18320faaf8771139d6
Anastacia Zhadan <anastaciazh@gmail.com>
2017-12-10 19:01:49 +0100

    render data for home page function added

--- public/js/index.js
+++ public/js/index.js
@@ -21,3 +21,48 @@ var renderCategorieData = function(data){
   }
   list.append("<li id=''><a class='center-align' href='/category' style='color: #000000'>show all</a></li>")
 };
+
+var renderData = function(data){
+    var products = data;
+
+
+    var temp = document.getElementById('productTemp'),
+        ul = document.getElementById('recent-manuals-list');
+
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+    console.log(products);
+    for(var k = 0; k < products.length; k++){
+        var clonedTemplate = temp.content.cloneNode(true);
+        clonedTemplate.querySelector('h6').innerText = products[k].productName;
+        clonedTemplate.querySelector('a').href = href='/product?id=' + products[k]._id;
+        clonedTemplate.querySelector('img').src = products[k].profilePicture;
+        clonedTemplate.querySelector('li').setAttribute('data-id', products[k]._id);
+
+
+        clonedTemplate.querySelector('.product-delete a.remove-selection').addEventListener("click",function () {
+            console.log(this);
+            var that = this;
+            var liElement = this.closest('li');
+            this.style.display = "none";
+            $.ajax({
+                url: 'api/v1/selected_product',
+                type: 'PUT',
+                data:{delete:true,product_id:liElement.getAttribute('data-id')},
+                success: function(res) {
+                    console.log(res);
+                    ul.removeChild(liElement);
+                },
+                error: function (err) {
+                    console.log(err.status);
+                    that.style.display = "";
+                    alert("Woops! Something went wrong, sry.")
+                }
+            });
+
+
+        });
+        ul.appendChild(clonedTemplate);
+    }
+};
