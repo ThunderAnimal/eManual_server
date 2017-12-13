5b74e5510ea067f3eb0d552bead10b3dd59c6335
PJ <prasannjeetsingh@gmail.com>
2017-12-13 04:15:58 +0100

    Extended the company model 2 accept list of consumers who opt in

--- app/models/Company.js
+++ app/models/Company.js
@@ -1,11 +1,15 @@
-"use strict"
+"use strict";
 
 var mongoose = require("mongoose");
 
 var companySchema = mongoose.Schema({
     login:{type:String,unique:true,requires:true},
     password:{type:String,requires:true},
-    name:{type: String,requires:true}
+    name:{type: String,requires:true},
+    isConsumerOptIn: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Consumer'
+    }],
 });
 
 module.exports = mongoose.model("Company", companySchema);
