5702d8b13fe07e96a0409217e1e9947782c91fd9
ThunderAnimal <MartinWeber.93@outlook.com>
2017-12-13 14:38:19 +0100

    show selection button on homepage

--- public/js/index.js
+++ public/js/index.js
@@ -51,12 +51,69 @@ var renderRecentProductsData = function(data){
         }
     }
     console.log(products);
+
     for(var k = 0; k < products.length; k++){
         var clonedTemplate = temp.content.cloneNode(true);
-        clonedTemplate.querySelector('h6').innerText = products[k].productName;
-        clonedTemplate.querySelector('a').href = href='/product?id=' + products[k]._id;
+        clonedTemplate.querySelector('.product-info h6').innerText = products[k].productName;
+        clonedTemplate.querySelector('.product-info a').href = href='/product?id=' + products[k]._id;
         clonedTemplate.querySelector('img').src = products[k].profilePicture;
         clonedTemplate.querySelector('li').setAttribute('data-id', products[k]._id);
+        if(products[k].isFavorite){
+            clonedTemplate.querySelector('a.add-selection').style.display= "none";
+            clonedTemplate.querySelector('a.remove-selection').style.display= "";
+        }else{
+            clonedTemplate.querySelector('a.add-selection').style.display= "";
+            clonedTemplate.querySelector('a.remove-selection').style.display= "none";
+        }
+
+        clonedTemplate.querySelector('a.add-selection').addEventListener("click",function () {
+            var that = this;
+            this.style.display = "none";
+            var e = this.parentNode.childNodes[2];
+            var pID =this.closest('li').getAttribute('data-id');
+            $.ajax({
+                url: 'api/v1/selected_product',
+                type: 'PUT',
+                data:{add:true,product_id:pID},
+                success: function(res) {
+                    console.log(res);
+                    e.style.display="";
+                },
+                error: function (err) {
+                    that.style.display = "";
+                    console.log(err);
+                    if(err.status === 401){
+                        alert("Log in is required for select product");
+                    }else if(err.status === 403){
+                        alert("Only consumer can select products");
+                    }
+                }
+            });
+        });
+        clonedTemplate.querySelector('a.remove-selection').addEventListener("click",function () {
+            var that = this;
+            this.style.display = "none";
+            var e = this.parentNode.childNodes[1];
+            var pID = this.closest('li').getAttribute('data-id');
+            $.ajax({
+                url: 'api/v1/selected_product',
+                type: 'PUT',
+                data: {delete: true, product_id: pID},
+                success: function (res) {
+                    console.log(res);
+                    e.style.display = "";
+                },
+                error: function (err) {
+                    that.style.display = "";
+                    console.log(err);
+                    if (err.status === 401) {
+                        alert("Log in is required for select product");
+                    } else if (err.status === 403) {
+                        alert("Only consumer can select products");
+                    }
+                }
+            });
+        });
 
         ul.appendChild(clonedTemplate);
     }
