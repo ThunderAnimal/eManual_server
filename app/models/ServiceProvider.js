34469051650ad110e958c4b548b101e25e3a13d0
eyuel <eyueltb@gmail.com>
2017-12-19 22:27:00 +0100

    "modified to contain many company"

--- app/models/ServiceProvider.js
+++ app/models/ServiceProvider.js
@@ -4,10 +4,10 @@ var serviceProviderSchema = mongoose.Schema({
     login:{type:String,unique:true,requires:true},
     password:{type:String,requires:true},
     name:{type: String,requires:true},
-    company_id: {
+    company_id: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Company'
-    }
+    }]
 });
 
 module.exports = mongoose.model("ServiceProvider", serviceProviderSchema);
