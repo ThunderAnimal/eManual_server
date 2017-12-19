93b8e6449b9718ff55a6ea23c74fb2541f9ce2fe
ThunderAnimal <MartinWeber.93@outlook.com>
2017-12-19 22:50:58 +0100

    removes unused code

--- public/js/category.js
+++ public/js/category.js
@@ -167,16 +167,6 @@ var renderNaviagtion = function(choosenCategories){
     catBreadCrump.show();
 };
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
 function getParams(url) {
     var regex = /([^=&?]+)=([^&#]*)/g, params = {}, parts, key, value;
 
