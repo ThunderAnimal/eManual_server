8d589fb40ae4a1fa7823ce7dc7371314dfbded6a
Anastacia Zhadan <anastaciazh@gmail.com>
2017-12-19 16:18:40 +0100

    categories added to header

--- public/js/ui-actions.js
+++ public/js/ui-actions.js
@@ -1,3 +1,26 @@
 $("#navigation li").click(function() {
     $( this ).addClass("sidebar-active");
 });
+
+$(document).ready(function(){
+    getCategorieData();
+});
+
+var getCategorieData = function(){
+    $.get('api/v1/categories_top', function(result){
+        renderCategorieData(result);
+    });
+};
+
+var renderCategorieData = function(data){
+    console.log(data);
+    var list=$("#dropdown1");
+
+    list.empty();
+    for(i=0;i<data.length;i++){
+        list.append("<li id='" + data[i]._id + "'><a href='/category?category_id[]=" + data[i]._id + "'>"+data[i].name + "<span class='badge'>"+data[i].count+"</span></a></li>");
+
+
+    }
+    list.append("<li id=''><a class='center-align' href='/category' style='color: #000000'>show all</a></li>")
+};
