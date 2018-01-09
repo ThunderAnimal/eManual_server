adee59a0ccfb38caffb81ed77f4c17ab88de24db
ThunderAnimal <MartinWeber.93@outlook.com>
2018-01-09 18:04:06 +0100

    Revert ""modify product schema""
    This reverts commit 7311226817ec2cdd1e50169778fb34f0ea2da162.

--- app/models/Product.js
+++ app/models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const productsSchema = mongoose.Schema({
-    productName: {type: String, text:true},
+    productName: {type: String},
     company_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Company'
@@ -13,21 +13,22 @@ const productsSchema = mongoose.Schema({
     }],
     productImages: [String],
     profilePicture: String,
-    productDescription:{type:String,requires:true},
+    productDescription:{type:String},
     favorites: {type: Number, default: 0},
     productResources: [{
-        description: {type: String, text:true},
-        originalName: {type: String, text:true},
+        description: {type: String},
+        originalName: {type: String},
         dataType: String,
         url: {type: String, requires: true}
     }],
     productLinks: [{
-        description: {type: String, text:true},
+        description: {type: String},
         url: {type: String, requires: true}
     }]
 }, {
 
-    timestamps: true
+    timestamps: true,
+    usePushEach: true
 
 });
 
@@ -35,11 +36,26 @@ productsSchema.index({"createdAt": 1});
 productsSchema.index({"updatedAt": 1});
 
 //SEARCH INDEX for FULL TEXT
-productsSchema.index({
-    productName: 'text',
-    'productResources.description': 'text',
-    'productResources.originalName': 'text',
-    'productLinks.description': 'text'
-});
+productsSchema.index(
+    {
+        "productName": "text",
+        "productDescription": "text",
+        "productResources.description": "text",
+        "productResources.originalName": "text",
+        "productLinks.description": "text"
+    },
+    {
+        name: "product.search_index"
+    },
+    {
+        "weights": {
+            "productName": 10,
+            "productDescription": 5,
+            "productResources.description": 2,
+            "productResources.originalName": 1,
+            "productLinks.description": 2
+        }
+    }
+);
 
 module.exports = mongoose.model("product", productsSchema);
