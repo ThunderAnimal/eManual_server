90f0d94946a10454958865deb640ae2a007ff1fd
ThunderAnimal <MartinWeber.93@outlook.com>
2018-01-09 11:12:36 +0100

    connect spamemail from service provider with api

---
+++ public/js/spamEmail.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+    $('#company').material_select();
+    getCompanyData();
+
+    $("form#sendMail").ajaxForm({
+        url: '/api/v1/service_provider/message',
+        success: function (res) {
+            console.log(res);
+            $('#sendMail').trigger("reset");
+            Materialize.toast('Mails send! Number: ' + res.count, 4000);
+
+        },
+        error: function (err) {
+            console.log(err);
+            if(err.responseJSON._error){
+                alert("Woops! Something went wrong: \n\n" + err.responseJSON.error);
+            }else{
+                alert("error");
+            }
+        }
+    });
+
+
+
+});
+
+var getCompanyData = function(){
+    //TODO GET REAL COMPANY LIST OF SERVICE PROVIDER
+    $.get('api/v1/categories', function(result){
+        renderCompanyData(result);
+    });
+};
+
+var renderCompanyData = function(data){
+    var list = $('#company');
+
+    for(var i = 0; i < data.length; i++){
+        list.append("<option value='" + data[i]._id + "' id='" + data[i]._id + "'>" + data[i].name  + "</option>");
+    }
+    list.material_select();
+
+};
